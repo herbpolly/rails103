78298b5adbb0431cf2db335e3d38322002fe6458
mayue <mayue@mayuedeMacBook-Air.local>
2017-04-06 19:31:53 +0800

    add bootstrap flash function

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,8 +1,8 @@
 module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
-  def flass_class(key)
-    FLASH_CLASS.fetch key.to_sym, key
+  def flash_class(key)
+    FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
